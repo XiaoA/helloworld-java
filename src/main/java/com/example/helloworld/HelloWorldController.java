45955438668aa0d64633c984bf07d50f8a0f16ba
Andrew Buckingham <abuckingham99@gmail.com>
2026-05-11 15:45:29 -0700

    Initial commit

---
+++ src/main/java/com/example/helloworld/HelloWorldController.java
@@ -0,0 +1,17 @@
+package com.example.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+public class HelloWorldController {
+
+  @GetMapping(path = "/hello")
+    public String helloWorld() {
+    return "Hello World, I love bacon!";
+  }
+}

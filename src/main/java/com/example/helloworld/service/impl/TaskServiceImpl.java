2f293fc9baff64931c34da4d229834ac5f92d866
Andrew Buckingham <abuckingham99@gmail.com>
2026-05-20 14:26:27 -0700

    Add `updateTask` method

--- src/main/java/com/example/helloworld/service/impl/TaskServiceImpl.java
+++ src/main/java/com/example/helloworld/service/impl/TaskServiceImpl.java
@@ -1,8 +1,10 @@
 package com.example.helloworld.service.impl;
 
 import com.example.helloworld.domain.CreateTaskRequest;
+import com.example.helloworld.domain.UpdateTaskRequest;
 import com.example.helloworld.domain.entity.Task;
 import com.example.helloworld.domain.entity.TaskStatus;
+import com.example.helloworld.exception.TaskNotFoundException;
 import com.example.helloworld.repository.TaskRepository;
 import com.example.helloworld.service.TaskService;
 import org.springframework.data.domain.Sort;
@@ -10,6 +12,7 @@ import org.springframework.stereotype.Service;
 
 import java.time.Instant;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class TaskServiceImpl implements TaskService {
@@ -40,5 +43,18 @@ public class TaskServiceImpl implements TaskService {
 	@Override
 	public List<Task> listTasks() {
 		return taskRepository.findAll(Sort.by(Sort.Direction.ASC, "created"));
+	}
+
+	@Override
+	public Task updateTask(UUID taskId, UpdateTaskRequest request) {
+		Task task = taskRepository.findById(taskId).orElseThrow(() -> new TaskNotFoundException(taskId));
+
+		task.setTitle(request.title());
+		task.setDescription(request.description());
+		task.setDueDate(request.dueDate());
+		task.setStatus(request.status());
+		task.setUpdated(Instant.now());
+
+		return taskRepository.save(task);
 	}
 }

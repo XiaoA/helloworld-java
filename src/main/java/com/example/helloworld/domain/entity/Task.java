31f973a338f742b86170ee92c91ab2baaf9e33be
Andrew Buckingham <abuckingham99@gmail.com>
2026-05-19 19:57:04 -0700

    Define Task Instance Variables

---
+++ src/main/java/com/example/helloworld/domain/entity/Task.java
@@ -0,0 +1,44 @@
+package com.example.helloworld.domain.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tasks")
+
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", updatable = false, nullable = false)
+    private UUID id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "description", length = 1000)
+    private String description;
+
+    @Column(name = "due_date")
+    private LocalDate dueDate;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    private TaskStatus status;
+
+    @Column(name = "created", updatable = false, nullable = false)
+    private Instant created;
+
+    @Column(name = "updated", updatable = false, nullable = false)
+    private Instant updated;
+}

8625dabfa7288cbdd44fd49f7b8322ec2da9c9ec
Andrew Buckingham <abuckingham99@gmail.com>
2026-05-26 23:18:34 -0700

    Refactor AccountController to use DTOs

--- src/main/java/com/example/helloworld/account/AccountController.java
+++ src/main/java/com/example/helloworld/account/AccountController.java
@@ -5,60 +5,56 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 @RestController
 @RequestMapping(path = "/api/v1/accounts")
 public class AccountController {
-
 	private final AccountService accountService;
 
 	public AccountController(AccountService accountService) {
 		this.accountService = accountService;
 	}
 
-	// Get All Accounts
-	@GetMapping("")
-	List<Account> findAll() {
-		return accountService.findAll();
+	@GetMapping
+	List<AccountDto> getAllAccounts() {
+		return accountService.findAll()
+			.stream()
+			.map(this::toAccountDto)
+			.toList();
 	}
 
-	// Get Account By Id
 	@GetMapping("/{accountId}")
-	Account findById(@PathVariable UUID accountId) {
-		Optional<Account> account = accountService.findById(accountId);
-		if (account.isEmpty()) {
-			throw new AccountNotFoundException(accountId);
-		}
-		return account.get();
+	AccountDto getAccountById(@PathVariable UUID accountId) {
+		Account account = accountService.findById(accountId)
+			.orElseThrow(() -> new AccountNotFoundException(accountId));
+
+		return toAccountDto(account);
 	}
 
-	// Create Account
 	@ResponseStatus(HttpStatus.CREATED)
 	@PostMapping
 	AccountDto createAccount(@Valid @RequestBody CreateAccountRequest request) {
-		Account account = accountService.createAccount(request);
-
-		return new AccountDto(
-			account.getId(),
-			account.getDisplayName(),
-			account.getAccountType()
-		);
+		return toAccountDto(accountService.createAccount(request));
 	}
 
-	// Update Account
 	@ResponseStatus(HttpStatus.OK)
 	@PutMapping("/{accountId}")
-	Account updateAccount(@PathVariable UUID accountId, @Valid @RequestBody UpdateAccountRequest request) {
-		return accountService.updateAccount(accountId, request);
+	AccountDto updateAccount(@PathVariable UUID accountId, @Valid @RequestBody UpdateAccountRequest request) {
+		return toAccountDto(accountService.updateAccount(accountId, request));
 	}
 
-	// Delete Account
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	@DeleteMapping("/{accountId}")
 	void deleteAccount(@PathVariable UUID accountId) {
 		accountService.deleteAccount(accountId);
 	}
 
+	private AccountDto toAccountDto(Account account) {
+		return new AccountDto(
+			account.getId(),
+			account.getDisplayName(),
+			account.getAccountType()
+		);
+	}
 }

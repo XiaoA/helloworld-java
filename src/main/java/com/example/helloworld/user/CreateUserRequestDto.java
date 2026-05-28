43683dc6d4931ccadd0c81a8f1388ceb31506b46
Andrew Buckingham <abuckingham99@gmail.com>
2026-05-28 08:03:26 -0700

    Add UserDto and UpdateUserRequest classes

---
+++ src/main/java/com/example/helloworld/user/CreateUserRequestDto.java
@@ -0,0 +1,20 @@
+package com.example.helloworld.user;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record CreateUserRequestDto(
+
+	@NotBlank(message = ERROR_MESSAGE_DISPLAY_NAME_REQUIRED)
+	String displayName,
+
+	@NotBlank(message = ERROR_MESSAGE_EMAIL_REQUIRED)
+	String email,
+
+	@NotNull(message = ERROR_MESSAGE_ROLE_REQUIRED)
+	UserRole role
+) {
+	public static final String ERROR_MESSAGE_DISPLAY_NAME_REQUIRED = "Display name is required";
+	public static final String ERROR_MESSAGE_EMAIL_REQUIRED = "Email is required";
+	public static final String ERROR_MESSAGE_ROLE_REQUIRED = "Role is required";
+}
---
+++ src/main/java/com/example/helloworld/user/UpdateUserRequest.java
@@ -0,0 +1,11 @@
+package com.example.helloworld.user;
+
+import com.example.helloworld.account.AccountType;
+
+public record UpdateUserRequest(
+		String displayName,
+		AccountType accountType
+) {
+}
+
+

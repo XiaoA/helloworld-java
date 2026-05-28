64ee2c6164701340a3939a19a3350fce2558e5ab
Andrew Buckingham <abuckingham99@gmail.com>
2026-05-28 08:04:25 -0700

    Add User Service

---
+++ src/main/java/com/example/helloworld/user/UserService.java
@@ -0,0 +1,74 @@
+package com.example.helloworld.user;
+
+import com.example.helloworld.account.Account;
+import com.example.helloworld.account.AccountNotFoundException;
+import com.example.helloworld.account.AccountRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class UserService {
+	private final UserRepository userRepository;
+	private final AccountRepository accountRepository;
+
+	public UserService(UserRepository userRepository, AccountRepository accountRepository) {
+		this.userRepository = userRepository;
+		this.accountRepository = accountRepository;
+	}
+
+	// Create User
+	@Transactional
+	public User createUser(UUID accountId, CreateUserRequestDto request) {
+		Account account = getAccount(accountId);
+		User user = new User();
+		user.setDisplayName(request.displayName());
+		user.setEmail(request.email());
+		user.setRole(request.role());
+		user.setAccount(account);
+		account.addUser(user);
+
+		return userRepository.save(user);
+	}
+
+	// List Users
+	public List<User> listUsers(UUID accountId) {
+		getAccount(accountId);
+		return userRepository.findAllByAccountId(accountId);
+	}
+
+	// Get User
+	public User getUser(UUID accountId, UUID userId) {
+		return userRepository.findByIdAndAccountId(userId, accountId)
+			.orElseThrow(() -> new UserNotFoundException(userId));
+	}
+
+	// Update User
+	public User updateUser(UUID accountId, UUID userId, UpdateUserRequestDto request) {
+		User user = getUser(accountId, userId);
+
+		user.setDisplayName(request.displayName());
+		user.setEmail(request.email());
+		user.setRole(request.role());
+
+		return userRepository.save(user);
+	}
+
+	// Delete User
+	public void deleteUser(UUID accountId, UUID userId) {
+		User user = getUser(accountId, userId);
+		userRepository.delete(user);
+	}
+
+	// Find All
+	public List<User> findAll() {
+		return userRepository.findAll();
+	}
+
+	private Account getAccount(UUID accountId) {
+		return accountRepository.findById(accountId)
+			.orElseThrow(() -> new AccountNotFoundException(accountId));
+	}
+}

cbb7ae4761e38186b11383bffaadd59199b5cf7f
Andrew Buckingham <abuckingham99@gmail.com>
2026-05-28 07:30:42 -0700

    Add UserRepository methods and fields

--- src/main/java/com/example/helloworld/user/UserRepository.java
+++ src/main/java/com/example/helloworld/user/UserRepository.java
@@ -1,7 +1,13 @@
 package com.example.helloworld.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
+	List<User> findAllByAccountId(UUID accountId);
+
+	Optional<User> findByIdAndAccountId(UUID userId, UUID accountId);
 }

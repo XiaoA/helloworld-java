994f15d67667644a63ba475fe25b233832a2e0d8
Andrew Buckingham <abuckingham99@gmail.com>
2026-05-28 17:06:55 -0700

    Update FormRepository.java

--- src/main/java/com/example/helloworld/form/FormRepository.java
+++ src/main/java/com/example/helloworld/form/FormRepository.java
@@ -2,7 +2,12 @@ package com.example.helloworld.form;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface FormRepository extends JpaRepository<Form, UUID> {
+	List<Form> findAllByAccountId(UUID accountId);
+
+	Optional<Form> findByIdAndAccountId(UUID formId, UUID accountId);
 }

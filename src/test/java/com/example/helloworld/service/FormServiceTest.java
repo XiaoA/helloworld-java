45ad1bbeacdc8c7fd0be8334b835995f6958a8a7
Andrew Buckingham <abuckingham99@gmail.com>
2026-05-29 22:03:50 -0700

    Update tests

--- src/test/java/com/example/helloworld/service/FormServiceTest.java
+++ src/test/java/com/example/helloworld/service/FormServiceTest.java
@@ -1,6 +1,7 @@
 package com.example.helloworld.service;
 
 import com.example.helloworld.model.dto.CreateFormRequestDto;
+import com.example.helloworld.model.dto.UpdateFormRequestDto;
 import com.example.helloworld.model.entity.Account;
 import com.example.helloworld.repository.AccountRepository;
 import com.example.helloworld.model.enums.AccountType;
@@ -12,6 +13,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -36,10 +39,13 @@ class FormServiceTest {
 		account.setAccountType(AccountType.REPRESENTATIVE);
 		account = accountRepository.save(account);
 
-		Form form = formService.createForm(account.getId(), new CreateFormRequestDto(
-			FormType.F1234,
-			"Gotham City Appeal"
-		));
+		Form form = formService.createForm(
+			account.getId(),
+			new CreateFormRequestDto(
+				FormType.F1234,
+				"Gotham City Appeal"
+			)
+		);
 
 		assertThat(form.getFormTitle()).isEqualTo("Gotham City Appeal");
 		assertThat(form.getFormType()).isEqualTo(FormType.F1234);
@@ -49,4 +55,37 @@ class FormServiceTest {
 		assertThat(account.getForms()).extracting(Form::getId).contains(form.getId());
 		assertThat(formRepository.findById(form.getId())).isPresent();
 	}
+
+	@Test
+	void updateFormUpdatesForm() {
+		Account account = new Account();
+		account.setDisplayName("Wayne Legal Group");
+		account.setAccountType(AccountType.REPRESENTATIVE);
+		account = accountRepository.save(account);
+
+		Form form = formService.createForm(account.getId(), new CreateFormRequestDto(
+			FormType.F1234,
+			"Gotham City Appeal"
+		));
+
+		assertThat(form.getFormTitle()).isEqualTo("Gotham City Appeal");
+		assertThat(form.getFormType()).isEqualTo(FormType.F1234);
+
+		formService.updateForm(account.getId(), form.getId(), new UpdateFormRequestDto(
+			FormType.F200,
+			"Updated Gotham City Appeal"
+		));
+
+		Form updatedForm = formRepository.findById(form.getId()).orElseThrow();
+
+		assertThat(updatedForm.getFormType()).isEqualTo(FormType.F200);
+		assertThat(updatedForm.getFormTitle()).isEqualTo("Updated Gotham City Appeal");
+	}
+
+//	@Test
+//	void getFormReturnsForm() {
+//		UUID uuid = UUID.randomUUID();
+//		Form form = FormRepository.findById(uuid).orElseThrow();
+//		assertThat(form).isNotNull();
+//	}
 }
